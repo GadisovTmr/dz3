98116c7f64a3fa136fa6c9d78b69481b688a10af
TimurGadisov <gadisovtmr@gmail.com>
2022-08-15 20:13:49 +0300

    задача 23

---
+++ dz23/Program.cs
@@ -0,0 +1,27 @@
+﻿ Console.WriteLine("Введите  N");
+int n = int.Parse(Console.ReadLine());
+for (int i=1; i<=n; i++)
+{
+    Console.Write ($"{i*i*i}  ");
+}
+
+
+
+
+
+
+
+
+// int n = int.Parse(Console.ReadLine());
+// int[] array =  new int [n];
+// int sqr;
+// int  index = 1;
+// int index2 = 0;
+// while (index != n-1);
+// {
+// sqr = index*index*index;
+// array[index2] = sqr;
+// index++;
+// index2++;
+// }
+// Console.WriteLine (array); 
