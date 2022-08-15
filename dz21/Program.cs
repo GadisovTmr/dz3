6542b5a9c9c562ee17fb1058dfdec3927b2b100a
TimurGadisov <gadisovtmr@gmail.com>
2022-08-15 20:13:58 +0300

    задача 21

---
+++ dz21/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите прогу, которая принимает на  вход координаты двух точек и находит расстрояние между ними в 3д 
+
+Console.Write ("Введите Х1 ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write ("Введите y1 ");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write ("Введите z1 ");
+int z1 = int.Parse(Console.ReadLine());
+Console.Write ("Введите Х2 ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write ("Введите y2 ");
+int y2 = int.Parse(Console.ReadLine());
+Console.Write ("Введите z2 ");
+int z2 = int.Parse(Console.ReadLine());
+double  distance = Math.Sqrt(Math.Pow (x1-x2,2) + Math.Pow (y1-y2,2) + Math.Pow (z1-z2,2));
+Console.WriteLine ("Дистанция равна " + distance);
