1335cb7f9ded8d9940950b526cdb2dd29a2b5758
TimurGadisov <gadisovtmr@gmail.com>
2022-08-18 19:51:13 +0300

    дз 27

---
+++ dz27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Прога принимает на вход число и  выдают сумму цифр в числе...
+
+
+Console.WriteLine("Введите  число");
+int n = int.Parse(Console.ReadLine());
+int indexA = n;
+int indexB = n;
+int sum = 0;
+while (indexB > 0)
+{
+    indexA = indexB % 10;
+    indexB = indexB / 10;
+    sum = sum + indexA;
+}
+
+Console.WriteLine(sum);
