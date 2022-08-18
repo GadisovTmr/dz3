7176e92dadc0b0bb6a63972eb0f5d5fd6c69653c
TimurGadisov <gadisovtmr@gmail.com>
2022-08-18 19:49:47 +0300

    дз 25

---
+++ dz25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Цикл  для возведения числа а в степень числа б
+
+
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+int sqr = a;
+if (b == 1) Console.WriteLine("равно " + sqr);
+else
+{
+    for (int i = 1; i < b; i++)
+    {
+        sqr = sqr * a;
+    }
+    Console.WriteLine("равно" + sqr);
+}
