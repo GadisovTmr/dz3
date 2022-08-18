3659430e8ca5b5b832d2d035d5ebb228e6d7b485
TimurGadisov <gadisovtmr@gmail.com>
2022-08-18 19:52:40 +0300

    дз 29

---
+++ dz29/Program.cs
@@ -0,0 +1,13 @@
+﻿// задать массив из 8 элементов и вывести на экран
+
+int[] massive = new int[8];
+int index = 0;
+while (index < 8)
+{
+    int value1 = new Random().Next(0, 100);
+    massive[index] = value1;
+    index++;
+}
+Console.Write(massive[0] + " , " + massive[1] + " , " + massive[2] + " , " + massive[3] + " , "
++ massive[4] + " , " + massive[5] + " , " + massive[6] + " , " + massive[7] + ".");
+
