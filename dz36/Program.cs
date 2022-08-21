7a4de944c3d1a65ac05f47f5d5045ed35ae3a5d2
TimurGadisov <gadisovtmr@gmail.com>
2022-08-21 22:56:46 +0300

    dz36

---
+++ dz36/Program.cs
@@ -0,0 +1,31 @@
+﻿//  Сумма элементов на нечетных позициях
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+
+}
+
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+int[] array = CreateRandomArray(5,1,100);
+int summa = 0;
+ShowArray (array);
+Console.WriteLine("----------------------------");
+for (int i = 0; i < array.Length; i++)
+{
+    if (i == 0) summa = summa + array[i];
+    else if  (i%2 == 0)  summa = summa + array[i];
+}
+Console.WriteLine(summa);
+
