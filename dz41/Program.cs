6ef465e62ce9898b98ff6eec2fe656307f60467f
TimurGadisov <gadisovtmr@gmail.com>
2022-08-25 16:54:20 +0300

    dz41

---
+++ dz41/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Сколько чисел вы собираетесь ввести?");
+    int m = int.Parse(Console.ReadLine());
+    int[] array = new int[m];
+    int count  = 0;
+    for (int i = 0; i < m; i++)
+    {
+     Console.WriteLine($"Введите {i + 1}  число");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]>0) count = count+1;
+    else  count = count;
+}
+Console.WriteLine("Количество чисел больше 0 = " + count );
---
+++ dz43/Program.cs
@@ -0,0 +1,20 @@
+﻿//   Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//   значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+int[,] matrix = new int[2, 2];
+
+Console.WriteLine("Введите b1");
+matrix[0, 0] = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите k1");
+matrix[0, 1] = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите b2");
+matrix[1, 0] = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите k2");
+matrix[1, 1] = int.Parse(Console.ReadLine());
+
+int crosspoint = (matrix[1, 0] - matrix[0, 0]) / (matrix[1, 1] - matrix[0, 1]);
+
+Console.WriteLine($"Точка пересечения  {crosspoint} ; {matrix[0, 1] * crosspoint + matrix[0, 0]}");
