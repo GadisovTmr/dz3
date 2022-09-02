b7ad64e132da79f93611629a3441a0599ddc38b2
TimurGadisov <gadisovtmr@gmail.com>
2022-09-02 21:08:15 +0300

    dz60

---
+++ dz60/Program.cs
@@ -0,0 +1,40 @@
+﻿// № 60 Трехмерный массив из неповторяющихся двухзначных чисел. 
+int[,,] array1 = new int [2,2,2];
+
+void Fillmatrix(int[,,] matr)
+{
+  
+for (int i = 0; i < matr.GetLength(0); i++)
+
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        for (int k = 0; k < matr.GetLength(2); k++)
+        {
+            matr[i,j,k] =matr[i,j,k]+ (k+1)+j*2+i*4+10;
+         }
+              
+          }
+        }
+    }
+    Console.WriteLine();
+
+void Printmatrix(int[,,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        for (int k = 0; k < matr.GetLength(2); k++)
+        {
+            Console.Write($"{matr[i,j,k]} ({i},{j},{k}) ");
+        }
+        Console.WriteLine();
+    }
+    
+}
+}
+
+Fillmatrix(array1);
+Printmatrix(array1);
