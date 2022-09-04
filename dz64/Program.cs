df1b3404bf6372b98c92e53a33ff65c1f1142b80
TimurGadisov <gadisovtmr@gmail.com>
2022-09-04 11:40:09 +0300

    dz64

---
+++ dz64/Program.cs
@@ -0,0 +1,14 @@
+﻿// 64 Значения м и Н и все натур числа между ними
+// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.WriteLine($"Введите М");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine($"Введите N");
+int n = int.Parse(Console.ReadLine());
+NumbersBetween(m,n);
+void NumbersBetween(int a, int b)
+{
+    Console.Write($" {a} ");
+if (a == b)  return;
+NumbersBetween(a+1,b); 
+}
