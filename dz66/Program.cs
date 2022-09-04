13d0a2e2fc5e40ad7e8ae0f75acad87b68daa397
TimurGadisov <gadisovtmr@gmail.com>
2022-09-04 11:40:17 +0300

    dz66

---
+++ dz66/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// 66 Задать м и н и сумма в их промежутке 
+
+Console.WriteLine($"Введите М");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine($"Введите N");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(SummBetween(m,n));
+
+int SummBetween(int a, int b)
+{
+int summ = 0;
+ if (a==b+1) return 0;
+ summ = summ +a+  SummBetween(a+1,b);
+ return summ;
+}
